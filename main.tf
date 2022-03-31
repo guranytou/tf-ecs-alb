adb9ac9554bb9bdf244751eca5192f3ccaa7dc9a
guranytou <guranytou@gmail.com>
2022-03-31 13:49:19 +0900

    test

--- main.tf
+++ main.tf
@@ -5,4 +5,13 @@ terraform {
       version = "4.5.0"
     }
   }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "aws-sundbox"
+
+    workspaces {
+      name = "tf-ecs-alb"
+    }
+  }
 }
